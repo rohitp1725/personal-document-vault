613e4734bd5b33262c7a9ae52c872a5c1c5411a6
Rohit Patil <rohit.p.1725@gmail.com>
2026-08-23 23:09:04 +0530

    Update Shareable.java

--- PersonalDocumentVault/src/interfaces/Shareable.java
+++ PersonalDocumentVault/src/interfaces/Shareable.java
@@ -4,3 +4,4 @@ public interface Shareable {
     boolean shareDocumentWith(String docId, String targetUsername, int durationDays);
     void revokeShare(String docId, String targetUsername);
 }
+//vhjgsdgjhghjgaskljdf
